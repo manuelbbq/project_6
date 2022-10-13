3f74e6c62e60cc79af71820bf5aa2393b69d533f
manuelbbq <man.martinez@bbqberlin.onmicrosoft.com>
2022-10-13 15:21:27 +0200

    add some crud

--- src/Controller/Usercontroller.php
+++ src/Controller/Usercontroller.php
@@ -2,25 +2,100 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserFormType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class Usercontroller extends AbstractController
 {
-    #[Route('/')]
+    #[Route('/', name: "app_login")]
     public function homepage ():Response
     {
         return $this->render('login/user.html.twig',[
         'title' => 'login'
         ]);
     }
-
-    #[Route('/browse/{user}')]
-    public function browse($user) :Response
+    #[Route('users/create', name: 'app_create_user')]
+    public function create(Request $request, EntityManagerInterface $entityManager) :Response
     {
-        return new Response('Hallo'.$user);
+        $user = new User();
+        $form = $this->createForm(UserFormType::class, $user);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $user = $form->getData();
+            $entityManager->persist($user);
+            $entityManager->flush();
 
+            return $this->redirectToRoute('app_users');
+        }
+
+
+        return $this->renderForm('user/new.html.twig',[
+            'form' => $form
+        ]);
+    }
+
+
+
+    #[Route('/users', name: 'app_users')]
+    public function index(UserRepository $userRepository) :Response
+    {
+        $users = $userRepository->findAll();
+        return $this->render('user/users.html.twig', ['users'=> $users]);
+    }
+
+
+    #[Route('users/store', name: 'app_store_user')]
+    public function store() :Response
+    {
+        return $this->render('user/users.html.twig');
+    }
+
+    #[Route('/users/{id}', name: 'app_user')]
+    public function show(User $user) :Response
+    {
+        return $this->render('user/user.html.twig',[
+            'user'=> $user]);
+
+    }
+
+
+
+
+
+    #[Route('users/{id}/edit', name: 'app_edit_user')]
+    public function edit(User $user): Response
+    {
+        return $this->render('user/edit.html.twig');
+    }
+    #[Route('users/{id}/delete', name: 'app_delete_user')]
+    public function delete(User $user):Response
+    {
+        return $this->render('user/users.html.twig');
+    }
+
+    #[Route('/new')]
+    public function newuser(EntityManagerInterface $entityManager) :Response
+    {
+        $user = new User();
+        $user->setName('Tim Thomsen');
+        $user->setOrt('timland');
+        $user->setEmail('tim@tamland.com');
+        $user->setPlz('10243');
+        $user->setTelefon('0190332332');
+        $user->setPasswort('geheim');
+
+        $entityManager->persist($user);
+        $entityManager->flush();;
+        return new Response(sprintf('Hallo %d du wohnst in %s',
+        $user->getId(),
+        $user->getOrt()));
     }
 
 }
---
+++ src/Entity/User.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\Email(
+        message: 'keine 132 Email'
+    )]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\Length(
+        min: 5,
+        max: 5,
+        exactMessage: 'PLZ muss 5 '
+    )]
+    private ?string $plz = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: 'Feld Ausfüllen')]
+    private ?string $ort = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: 'Feld Ausfüllen')]
+    private ?string $telefon = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: 'Feld Ausfüllen')]
+    private ?string $passwort = null;
+
+    #[ORM\Column]
+    private \DateTimeImmutable $createdAt;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+    }
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPlz(): ?string
+    {
+        return $this->plz;
+    }
+
+    public function setPlz(string $plz): self
+    {
+        $this->plz = $plz;
+
+        return $this;
+    }
+
+    public function getOrt(): ?string
+    {
+        return $this->ort;
+    }
+
+    public function setOrt(string $ort): self
+    {
+        $this->ort = $ort;
+
+        return $this;
+    }
+
+    public function getTelefon(): ?string
+    {
+        return $this->telefon;
+    }
+
+    public function setTelefon(string $telefon): self
+    {
+        $this->telefon = $telefon;
+
+        return $this;
+    }
+
+    public function getPasswort(): ?string
+    {
+        return $this->passwort;
+    }
+
+    public function setPasswort(string $passwort): self
+    {
+        $this->passwort = $passwort;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+
+}
